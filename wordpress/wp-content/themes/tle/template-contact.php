8b0ab3d29be6c77cc240073412dc5da5b997b902
Xubi Nguyen <xubinguyen@gmail.com>
2015-05-13 00:06:28 +0700

    tạo trang liên hệ

--- wordpress/wp-content/themes/tle/template-contact.php
+++ wordpress/wp-content/themes/tle/template-contact.php
@@ -7,4 +7,21 @@
 <?php get_header();?>
 
 
-<?php get_footer();?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+  
+  	<div class="contact-map"><?php echo do_shortcode('[wc_googlemap title="' . get_field('map_title') .'" location="'. get_field('address') .'" zoom="14" title_on_load="no" height="300" class=""]');?></div>
+  		<div class="rows">
+     <?php
+	  while ( have_posts() ) : the_post();
+	  ?>
+	  <?php the_content();?>
+	    <?php
+	  endwhile;
+	?>
+</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
