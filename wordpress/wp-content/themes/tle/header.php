9f9289b5aa32a3ad5420a341e4fa8ba0666bdad7
nguyenanhdung83 <nguyenanhdung83@gmail.com>
2015-05-12 22:28:05 +0700

    header css

--- wordpress/wp-content/themes/tle/header.php
+++ wordpress/wp-content/themes/tle/header.php
@@ -13,24 +13,55 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo bloginfo('template_url'); ?>/assets/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="<?php echo bloginfo('template_url'); ?>/assets/css/jquery.bxslider.css">
+<link rel="stylesheet" type="text/css" href="<?php echo bloginfo('template_url'); ?>/assets/css/main.css">
 
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'xc' ); ?></a>
-
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'xc' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'top' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+	<header>
+			<div class="bgHeader"><img src="<?php echo bloginfo('template_url'); ?>/assets/img/bg-header.jpg" alt="TLE bg"></div>
+			<div class="wrap">
+				<div class="logo">
+					<a href="<?php echo bloginfo('url'); ?>">
+						<img src="<?php echo bloginfo('template_url'); ?>/assets/img/logo.png" alt="TLE">
+					</a>
+				</div>
+			</div>
+			<div class="wrap">
+				<div class="searchbox">
+					<form action="">
+						<input type="text" id="search" name="txtSearch" class="txtSearch" />
+						<input type="submit" id="submit" class="iconSearch" />
+					</form>
+				</div>  
+			</div>
+			<div class="mainNav">
+				<div class="wrap">
+					<ul class="language">
+						<li><a href="#"><img src="<?php echo bloginfo('template_url'); ?>/assets/img/icon-en.jpg" alt="English"></a></li>
+						<li><a href="#"><img src="<?php echo bloginfo('template_url'); ?>/assets/img/icon-vi.jpg" alt="Tiếng Việt"></a></li>
+					</ul>
+					<?php
+						wp_nav_menu( array(
+							'menu'    => 'top',
+							'walker'  => new themeslug_walker_nav_menu_pc()
+						));
+					?>
+				</div>
+			</div>
+			<div class="path">
+				<div class="container">
+					<ul>
+						<li><a href="#">Trang chủ</a></li>
+						<li>Tin tức</li>
+					</ul>
+					<h1 class="mainTitle">Tin tức</h1>
+				</div>        
+			</div>
+		</header>
 
 	<div id="content" class="site-content">
